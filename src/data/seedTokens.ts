80f46836dc08f3a01f9a3ff23884813cb7e1e313
Kanishka Saket <kanishkasaket1007@gmail.com>
2026-01-10 15:53:29 +0530

    added tokens

--- src/data/seedTokens.ts
+++ src/data/seedTokens.ts
@@ -2,5 +2,14 @@ export const seedTokens = [
   {
     address: "So11111111111111111111111111111111111111112",
     symbol: "SOL"
+  },
+  {
+    address: "EPjFWdd5AufqSSqeM2qZ3i9KqTj7fM9J6r4Zy7xX",
+    symbol: "USDC"
+  },
+  {
+    address: "Es9vMFrzaCERn7ZB6t8uE7tF6pMZ9YyXz6Z3wX",
+    symbol: "USDT"
   }
 ];
+
